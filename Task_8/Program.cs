995bce598ee21cdc3b1b3bdf4ac72593ea7c37ad
kirilllavrov <5860107+kirilllavrov@users.noreply.github.com>
2022-11-27 23:41:18 +0300

    Update Program.cs

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введи число: ");
+Console.WriteLine("Введи число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int count = 0;
 int even = 0;
@@ -6,7 +6,7 @@ int even = 0;
 while (count < N)
 {
     even = even + 1;
-    if (even % 2 == 0) Console.Write(even + ", ");
+    if (even % 2 == 0) Console.Write(even);
     if (even % 2 == 0 && even < N-1) Console.Write(", ");
     count++;
 }
