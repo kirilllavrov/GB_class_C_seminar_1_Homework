45b90390c801934f5f2c0a5b42431c60c0cd8f27
Kirill Lavrov <kirill.lavrov@checloud.ru>
2022-11-27 21:25:58 +0300

    add Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿Console.WriteLine("Введи первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if(numberA > max ) max = numberA;
+if(numberB > max ) max = numberB;
+if(numberC > max ) max = numberC;
+
+Console.WriteLine(max);
