bbc2240ed759beeea30c365db1a1c7de50303525
Kirill Lavrov <kirill.lavrov@checloud.ru>
2022-11-27 21:36:00 +0300

    add Task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿﻿Console.WriteLine("Введи число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
