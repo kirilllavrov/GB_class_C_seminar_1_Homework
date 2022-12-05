5fffbe84a4582e768359a4cc2cdd491be5c84264
Kirill Lavrov <kirill.lavrov@checloud.ru>
2022-12-05 19:49:41 +0300

    Внес правки преподавателя и исправил конфликт

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,11 +1,12 @@
-Console.WriteLine("Введи число: ");
+﻿Console.WriteLine("Введи число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int count = 0;
 //int even = 0;       // Можно обойтись одной переменной, убираем even
 
 while (count <= N)  // Подправил под одну переменную
 {
-    even = even + 1;
-    if (even % 2 == 0) Console.Write(even + ", ");
+    //even = even + 1;                                              // убираем лишнюю переменную
+    if (count % 2 == 0) Console.Write(count);                       // Заменил even на count
+    if (count % 2 == 0 && count < N) Console.Write(", ");           // Заменил even на count
     count++;
 }
