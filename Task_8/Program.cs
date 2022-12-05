aa26ed0ca2893783f437223afb1d32149574e7d2
Kirill Lavrov <kirill.lavrov@checloud.ru>
2022-12-05 19:24:52 +0300

    Edit

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -6,6 +6,7 @@ int even = 0;
 while (count < N)
 {
     even = even + 1;
-    if (even % 2 == 0) Console.Write(even + ", ");
+    if (even % 2 == 0) Console.Write(even);
+    if (even % 2 == 0 && even < N-1) Console.Write(", ");
     count++;
 }
